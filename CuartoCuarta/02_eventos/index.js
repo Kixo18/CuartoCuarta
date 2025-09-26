23b5205a14b5a64d478356d53448eb4ddbba77dc
Kixo18 <gombenja51@gmail.com>
2025-09-26 16:37:55 -0300

    Add files via upload

---
+++ CuartoCuarta/02_eventos/index.js
@@ -0,0 +1,10 @@
+// Creamos la funcioon para Cerrar Sesión
+function cerrarSesion() {
+    
+    alert("Cerraste sesión, gracias por la visita");
+}
+
+    // Función para salir de la página
+    function salirPagina() {
+    location.href = "https://www.google.com";
+    }
---
+++ CuartoCuarta/05_funciones/index.js
@@ -0,0 +1,12 @@
+// Ejercicios del práctico 05 - Funciones 02
+console.log("==============================");
+console.log("Practico 06 - Funciones 02");
+console.log("==============================");
+
+// Función nativa de JavaScript
+function saludar() {
+    console.log("Buenas tardes, soy la función saludar");
+}
+
+// para ejecutar la función debemos llamarla
+saludar();
